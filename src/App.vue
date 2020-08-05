1135cbf725c03c4146d41cfb25bc9ae2e4d4adc4
Proladon <proladon@gmail.com>
2020-08-05 15:52:22 +0800

    add electron-builder

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Viewer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Viewer from './components/Viewer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Viewer,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
 }
 </style>
---
+++ src/components/Viewer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="viewer">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Viewer',
+}
+</script>
+
+<style scoped lang="scss">
+#viewer{
+    background-color: cadetblue;
+}
+</style>
