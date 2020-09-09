2186d50f639a7f413858a160daac3abb66310b0e
Proladon <proladon@gmail.com>
2020-09-09 22:23:09 +0800

    remove sidebar unuse shit

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 const state = {
     isChanged: false,
 
@@ -7,23 +7,11 @@ const state = {
     workspace: {
         name: "untitled",
         path: "",
-    },
+    }
     
-    // views: {
-    //     workspacesView: false,
-    //     infoView: false,
-    //     updateView: false,
-    //     settingsView: false,
-    // },
 }
 
 const mutations = {
-    CHANGE_VIEW: (state, view) => {
-        Object.keys(state.views).map(v => {
-            state.views[v] = false
-        })
-        Vue.set(state.views, view, true)
-    },
 
     SET_VIEW: (state, view) => {
         state.curView = view
@@ -31,12 +19,6 @@ const mutations = {
 
     SET_WORKSPACE: (state, workspace) => {
         state.workspace = workspace
-    },
-
-    HOME_VIEW: (state) => {
-        Object.keys(state.views).map(v => {
-            state.views[v] = false
-        })
     }
 }
 
